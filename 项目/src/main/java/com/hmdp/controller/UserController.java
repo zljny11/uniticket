cdef5db231fb18b2d72c813b7469b2f5d90f5097
zljny <zljny11@outlook.com>
2025-12-23 23:29:01 +0800

    login

--- 项目/src/main/java/com/hmdp/controller/UserController.java
+++ 项目/src/main/java/com/hmdp/controller/UserController.java
@@ -6,6 +6,7 @@ import com.hmdp.dto.Result;
 import com.hmdp.entity.UserInfo;
 import com.hmdp.service.IUserInfoService;
 import com.hmdp.service.IUserService;
+import com.hmdp.service.impl.UserServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
@@ -37,7 +38,7 @@ public class UserController {
     @PostMapping("code")
     public Result sendCode(@RequestParam("phone") String phone, HttpSession session) {
         // TODO 发送短信验证码并保存验证码
-        return Result.fail("功能未完成");
+        return userService.sendCode(phone, session);
     }
 
     /**
@@ -46,8 +47,7 @@ public class UserController {
      */
     @PostMapping("/login")
     public Result login(@RequestBody LoginFormDTO loginForm, HttpSession session){
-        // TODO 实现登录功能
-        return Result.fail("功能未完成");
+        return userService.login(loginForm, session);
     }
 
     /**
--- 项目/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ 项目/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,20 +1,67 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-/**
- * <p>
- * 服务实现类
- * </p>
- *
- * @author 虎哥
- * @since 2021-12-22
- */
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
+
+@Slf4j
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        //校验手机号
+        // 如果不符合返回错误信息
+        if (RegexUtils.isPhoneInvalid(phone)){return Result.fail("phone number format error!");}
+        //如果符合就生成验证码
+        String code = RandomUtil.randomNumbers(6);
+        //保存验证码到session
+        session.setAttribute("code",code);
+        //发送验证码（简易）
+        log.debug("send code:{} to phone:{}",code, phone);
+        return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        //校验手机号
+        String phone = loginForm.getPhone();
+        if (RegexUtils.isPhoneInvalid(phone)){return Result.fail("phone number format error!");}
+        //校验验证码
+        String cacheCode = (String) session.getAttribute("code");
+        String code = loginForm.getCode();
+        if (cacheCode == null || !cacheCode.equals(code)){return Result.fail("code error!");}
+        //根据手机号查询用户 SELECT * FROM tb_user WHERE phone = ?
+        User user = query().eq("phone", phone).one();
+        //判断用户是否存在
+        if (user == null) {
+            //不存在，创建新用户并保存
+            user = createUserWithPhone(phone);
+        }
+        //保存用户信息到session
+        session.setAttribute("user", user);
+        return Result.ok();
+    }
+
+    private User createUserWithPhone(String phone) {
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomString(10));
+        save(user);
+        return user;
+    }
 }
